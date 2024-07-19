8f167475d42ba37b57025650347ae17fffed29f3
n00b St4r <realgoldcraft@gmail.com>
2024-07-19 23:54:55 +0700

    change to ssg

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,11 @@
+import type { LayoutServerLoad } from './$types';
+
+export const prerender = true;
+
+export const load: LayoutServerLoad = async ({ url }) => {
+  const { pathname } = url;
+
+  return {
+    pathname
+  };
+};
--- src/routes/+layout.ts
+++
@@ -1,9 +0,0 @@
-import type { LayoutLoad } from './$types';
-
-export const load: LayoutLoad = async ({ url }) => {
-  const { pathname } = url;
-
-  return {
-    pathname
-  };
-};
